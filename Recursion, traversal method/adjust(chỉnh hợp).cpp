80bfb6fe451005478687a63051af725093f4c06a
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2025-02-05 15:38:33 +0700

    Create adjust(chỉnh hợp).cpp

---
+++ Recursion, traversal method/adjust(chỉnh hợp).cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+#define max 100
+string str[5]={"0","a","b","c","d"}; 
+int a[max], n, k;
+bool isNotChosen[max];
+void createBoolArray(bool a[], int n){
+	for(int i=1;i<=n;i++)
+		isNotChosen[i]=true;
+}
+void displayArray(int a[], int n){
+	for(int i=1;i<=n;i++)
+		cout<<str[a[i]]<<" ";
+	cout<<endl;
+}
+void Try(int i){
+	for(int j=1;j<=n;j++)
+	{
+		if(isNotChosen[j]){
+			a[i]=j;
+			if(i==k)
+				displayArray(a,k);
+			else
+			{
+				isNotChosen[j]=false;
+				Try(i+1);
+				isNotChosen[j]=true;
+			}
+		}
+	}
+}
+int main(int argc, char *argv[])
+{
+	cin>>n>>k;
+	createBoolArray(isNotChosen,n);
+	Try(1);
+	return 0;
+}
